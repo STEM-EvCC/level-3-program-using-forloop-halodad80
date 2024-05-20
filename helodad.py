dfebb906a521f278af8f74e8ed4610a0a78c7b4a
halodad80 <166643978+halodad80@users.noreply.github.com>
2024-05-20 23:28:29 +0000

    halo

---
+++ helodad.py
@@ -0,0 +1,17 @@
+ #iterating over a list
+
+my_list = [69, 420, 6969, 5000, 1.3598]
+for item in my_list:
+    print(item)
+
+
+    # Iterating over a tuple
+my_tuple = ('f', 'u', 'c', 'k',)
+for item in my_tuple:
+    print(item)
+
+
+# Iterating over a string
+my_string = "yo"
+for char in my_string:
+    print(char)
